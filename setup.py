e80ba4d1239ca66dca0cd3dafe096fd06aba6f4f
OwenFeik <owenhfeik@gmail.com>
2021-06-25 14:33:23 +1000

    add download url, requirements

--- setup.py
+++ setup.py
@@ -8,4 +8,6 @@ setuptools.setup(
     author_email="owen.h.feik@gmail.com",
     description="Utility to download Archidekt decklists for local use.",
     packages=setuptools.find_packages(),
+    download_url="https://github.com/OwenFeik/architrice/archive/refs/tags/0.0.1.tar.gz",
+    install_requires=["requests"]
 )
