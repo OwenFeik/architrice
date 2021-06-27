8abe8175ce9754cfbaa72c43f082dd8421e70bfc
OwenFeik <owenhfeik@gmail.com>
2021-06-27 18:59:06 +1000

    allow partial target descriptions

--- architrice/main.py
+++ architrice/main.py
@@ -20,23 +20,24 @@ import architrice.cockatrice as cockatrice
 APP_NAME = "architrice"
 
 DESCRIPTION = f"""
-Download Archidekt decks to a local directory. To set output path:
-{APP_NAME} -p OUTPUT_DIRECTORY
-This is cached and will be used until a different path is set.
+{APP_NAME} is a tool to download decks from online sources to local directories.
+To set up, run {APP_NAME} with no arguments. This will run a wizard to set up a
+link between an online source and a local directory. Future runs of {APP_NAME}
+will then download all decklists that have been updated or created since the
+last run to that directory. 
 
-To download a single deck to the set output path:
-{APP_NAME} -d ARCHIDEKT_DECK_ID
-This deck id is cached, and if the command is run again without argument, the
-same deck will be downloaded.
+To add another download target beyond this first one, run {APP_NAME} -n.
 
-To download all decks for a specific user name:
-{APP_NAME} -u ARCHIDEKT_USERNAME
-This username is cached, and if the command is run again without argument, the
-same user's decks will be downloaded.
+To delete an existing download target, run {APP_NAME} -d, which will launch a
+wizard to do so.
 
-To download the most recently updated deck for a specific user:
-{APP_NAME} -l
-If no user has been set, the user will need to be specified as well through -u.
+To download only the most recently updated decklist for each target, run
+{APP_NAME} -l.
+
+To set up a new target or delete a target without CLI, use the flags for source,
+user and path as in 
+{APP_NAME} -s SOURCE -u USER -p PATH -n
+Replace -n with -d to delete instead of creating. 
 """
 
 SOURCES = {
@@ -53,6 +54,16 @@ SOURCES = {
 }
 
 
+def get_source(name, picker=False):
+    if name in SOURCES:
+        return SOURCES[name]
+    else:
+        for s in SOURCES.values():
+            if name in s["names"]:
+                return s
+    return None
+
+
 def source_picker():
     return SOURCES[
         cli.get_choice(
@@ -63,52 +74,78 @@ def source_picker():
     ]
 
 
-def add_source(cache):
-    source = source_picker()
+def add_source(cache, source=None, user=None, path=None):
+    source = get_source(source, True)
     target = cockatrice
 
-    while not source["api"].verify_user(
-        (user := cli.get_string(source["name"] + " username"))
-    ):
-        print("Couldn't find any public decks for this user. Try again.")
+    if user:
+        if not source["api"].verify_user(user):
+            logging.error(f"Couldn't verify user {user}. Ignoring new target.")
+            return
+    else:
+        while not source["api"].verify_user(
+            (user := cli.get_string(source["name"] + " username"))
+        ):
+            print("Couldn't find any public decks for this user. Try again.")
 
-    if cache["dirs"] and cli.get_decision("Use existing output directory?"):
-        if len(cache["dirs"]) == 1:
-            path = list(cache["dirs"].keys())[0]
-        else:
-            path = cli.get_choice(
-                list(cache["dirs"].keys()),
-                "Which existing directory should be used for these decks?",
+    if path:
+        if not utils.check_dir(path):
+            logging.error(
+                f"A file exists at {path} so it can't be used as an output "
+                "directory."
             )
     else:
-        path = target.suggest_directory()
-        if not (
-            (os.path.isdir(path))
-            and cli.get_decision(
-                f"Found Cockatrice deck directory at {path}."
-                " Output decklists here?"
-            )
-        ):
-            path = cli.get_path("Output directory")
+        if cache["dirs"] and cli.get_decision("Use existing output directory?"):
+            if len(cache["dirs"]) == 1:
+                path = list(cache["dirs"].keys())[0]
+            else:
+                path = cli.get_choice(
+                    list(cache["dirs"].keys()),
+                    "Which existing directory should be used for these decks?",
+                )
+        else:
+            path = target.suggest_directory()
+            if not (
+                (os.path.isdir(path))
+                and cli.get_decision(
+                    f"Found Cockatrice deck directory at {path}."
+                    " Output decklists here?"
+                )
+            ):
+                path = cli.get_path("Output directory")
 
     if cache["sources"].get(source["name"]) is None:
         cache["sources"][source["name"]] = []
     cache["sources"][source["name"]].append({"user": user, "dir": path})
 
 
-def delete_source(cache):
+def delete_source(cache, source=None, user=None, path=None):
+    source = get_source(source)
+
     options = []
     for s in cache["sources"]:
+        if source and not source["name"] == s:
+            continue
+
         for t in cache["sources"][s]:
-            user = t["user"]
-            path = t["dir"]
-            options.append(f"{s}: {user} ({path})")
+            t_user = t["user"]
+            t_path = t["dir"]
+
+            if user and t_user != user:
+                continue
+            if path and not os.path.samefile(t_path, path):
+                continue
+            options.append(f"{s}: {t_user} ({t_path})")
 
     if not options:
         logging.info("No targets exist, ignoring delete option.")
         return
+    elif len(options) == 1:
+        logging.info("One target matches criteria, deleting this.")
+        target = options[0]
+    else:
+        target = cli.get_choice(options, "Delete which target?")
 
-    target = cli.get_choice(options, "Delete which target?")
     m = re.match(r"^(?P<source>\w+): (?P<user>\w+) \((?P<path>.+)\)$", target)
     source = m.group("source")
     user = m.group("user")
@@ -186,7 +223,7 @@ def main():
         cache = utils.DEFAULT_CACHE
         add_source(cache)
     elif args.new:
-        add_source(cache)
+        add_source(cache, args.source, args.user, args.path)
 
     if args.source and args.path and args.user:
         logging.info(
@@ -200,7 +237,7 @@ def main():
         )
 
     if args.delete:
-        delete_source(cache)
+        delete_source(cache, args.source, args.user, args.path)
 
     for source in cache["sources"]:
         api = SOURCES[source]["api"]
