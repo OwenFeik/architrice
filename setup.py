d9e4f0b16cf8ac093be16b226fef9f99b67977eb
OwenFeik <owenhfeik@gmail.com>
2021-08-22 16:19:32 +1000

    add description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="architrice",
     version="0.0.4",
@@ -7,6 +10,8 @@ setuptools.setup(
     author="Owen Feik",
     author_email="owen.h.feik@gmail.com",
     description="Utility to MtG decklists from online sources.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     download_url="https://github.com/OwenFeik/architrice/archive/refs/tags/0.0.4.tar.gz",
     install_requires=["requests", "bs4"],
