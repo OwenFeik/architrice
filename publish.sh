66fc7f7df8c66073c207b5d76c6ced8c394aaade
OwenFeik <owenhfeik@gmail.com>
2021-11-10 15:43:23 +1100

    just use sdist

--- publish.sh
+++ publish.sh
@@ -4,7 +4,6 @@ rm "$dir/dist/"* &> /dev/null
 
 python -m pip install -r "$dir/devreqs.txt" > /dev/null
 python setup.py sdist
-python setup.py bdist_wheel
 twine upload dist/*
 
 echo "Add dist/*.tar.gz as a release on GitHub."
